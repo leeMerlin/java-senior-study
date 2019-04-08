2336c66478ae762e60c5536945a9487f7df41194
leeMerlin <1666154133@qq.com>
2019-04-08 12:31:36 +0800

    多线程

---
+++ swing-study/src/com/soft1841/swing/RunnableTest.java
@@ -0,0 +1,25 @@
+package com.soft1841.swing;
+
+/**
+ * RunnableTest多线程
+ */
+public class RunnableTest extends Object implements Runnable {
+    private int count = 10;
+    //重写run方法
+    @Override
+    public void run() {
+        while (true) {
+            //打印count变量
+            System.out.print(count + " ");
+            if (--count == 0) {
+                break;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        //创建线程对象
+        RunnableTest test = new RunnableTest();
+        //启动线程
+        test.run();
+    }
+}
